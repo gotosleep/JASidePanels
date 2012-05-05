4f00e54049b9585c129462a40b45ab7585e39904
Sam Soffes <sam@samsoff.es>
2012-05-05 12:35:15 -0700

    Cache default image

--- JASidePanels/Source/JASidePanelController.m
+++ JASidePanels/Source/JASidePanelController.m
@@ -111,21 +111,26 @@
 #pragma mark - Icon
 
 + (UIImage *)defaultImage {
-    UIGraphicsBeginImageContextWithOptions(CGSizeMake(20.f, 13.f), NO, 0.0f);
-    
-    [[UIColor blackColor] setFill];
-    [[UIBezierPath bezierPathWithRect:CGRectMake(0, 0, 20, 1)] fill];
-    [[UIBezierPath bezierPathWithRect:CGRectMake(0, 5, 20, 1)] fill];
-    [[UIBezierPath bezierPathWithRect:CGRectMake(0, 10, 20, 1)] fill];
-    
-    [[UIColor whiteColor] setFill];
-    [[UIBezierPath bezierPathWithRect:CGRectMake(0, 1, 20, 2)] fill];
-    [[UIBezierPath bezierPathWithRect:CGRectMake(0, 6,  20, 2)] fill];
-    [[UIBezierPath bezierPathWithRect:CGRectMake(0, 11, 20, 2)] fill];   
-    
-    UIImage *iconImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    return iconImage;
+	static UIImage *defaultImage = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		UIGraphicsBeginImageContextWithOptions(CGSizeMake(20.f, 13.f), NO, 0.0f);
+		
+		[[UIColor blackColor] setFill];
+		[[UIBezierPath bezierPathWithRect:CGRectMake(0, 0, 20, 1)] fill];
+		[[UIBezierPath bezierPathWithRect:CGRectMake(0, 5, 20, 1)] fill];
+		[[UIBezierPath bezierPathWithRect:CGRectMake(0, 10, 20, 1)] fill];
+		
+		[[UIColor whiteColor] setFill];
+		[[UIBezierPath bezierPathWithRect:CGRectMake(0, 1, 20, 2)] fill];
+		[[UIBezierPath bezierPathWithRect:CGRectMake(0, 6,  20, 2)] fill];
+		[[UIBezierPath bezierPathWithRect:CGRectMake(0, 11, 20, 2)] fill];   
+		
+		defaultImage = UIGraphicsGetImageFromCurrentImageContext();
+		UIGraphicsEndImageContext();
+
+	});
+    return defaultImage;
 }
 
 #pragma mark - NSObject
