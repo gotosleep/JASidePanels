e8c6731e51bea0b1dcebdfe62e8d22c00d0fdbde
Ivan <ivan.trufanov@me.com>
2013-03-03 10:00:37 +0400

    Shows other center, then old hidden
    Now it crashes, if you will try to show new center, then old hidden

--- JASidePanels/Source/JASidePanelController.m
+++ JASidePanels/Source/JASidePanelController.m
@@ -354,18 +354,26 @@ static char ja_kvoContext;
     if (self.isViewLoaded && self.state == JASidePanelCenterVisible) {
         [self _swapCenter:previous with:_centerPanel];
     } else if (self.isViewLoaded) {
-        // update the state immediately to prevent user interaction on the side panels while animating
-        JASidePanelState previousState = self.state;
-        self.state = JASidePanelCenterVisible;
-        [UIView animateWithDuration:0.2f animations:^{
-            // first move the centerPanel offscreen
-            CGFloat x = (previousState == JASidePanelLeftVisible) ? self.view.bounds.size.width : -self.view.bounds.size.width;
-            _centerPanelRestingFrame.origin.x = x;
-            self.centerPanelContainer.frame = _centerPanelRestingFrame;
-        } completion:^(__unused BOOL finished) {
-            [self _swapCenter:previous with:_centerPanel];
-            [self _showCenterPanel:YES bounce:NO];
-        }];
+		if (_centerPanelHidden) {
+			[self _unhideCenterPanel];
+			_centerPanelHidden = NO;
+			
+			[self _swapCenter:previous with:_centerPanel];
+			[self _showCenterPanel:YES bounce:NO];
+		} else {
+			// update the state immediately to prevent user interaction on the side panels while animating
+			JASidePanelState previousState = self.state;
+			self.state = JASidePanelCenterVisible;
+			[UIView animateWithDuration:0.2f animations:^{
+				// first move the centerPanel offscreen
+				CGFloat x = (previousState == JASidePanelLeftVisible) ? self.view.bounds.size.width : -self.view.bounds.size.width;
+				_centerPanelRestingFrame.origin.x = x;
+				self.centerPanelContainer.frame = _centerPanelRestingFrame;
+			} completion:^(__unused BOOL finished) {
+				[self _swapCenter:previous with:_centerPanel];
+				[self _showCenterPanel:YES bounce:NO];
+			}];
+		}
     }
 }
 
