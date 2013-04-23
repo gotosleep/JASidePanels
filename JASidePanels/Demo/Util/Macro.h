6315d117fa838f6d874b657a9701466f13894802
sunkehappy <675313675@qq.com>
2013-04-23 23:04:27 +0800

    Update Macro.h
    Correct error. Refer to this answer at SO:http://stackoverflow.com/a/969291/1548523

--- JASidePanels/Demo/Util/Macro.h
+++ JASidePanels/Demo/Util/Macro.h
@@ -13,12 +13,12 @@
 #ifdef DEBUG
 
 #define JAPrintBaseLog      NSLog(@"%s %@ [line %d]", __FUNCTION__, self, __LINE__)
-#define JALog(format, ...)  NSLog(@"%s %@ [line %d]", __PRETTY_FUNCTION__, self, __LINE__, ##__VA_ARGS_)
+#define JALog(format, ...)  NSLog((@"%s %@ [line %d] " format), __PRETTY_FUNCTION__, self, __LINE__, ##__VA_ARGS__)
 
 #else
 
 #define JAPrintBaseLog ;
-#define JALog;
+#define JALog(...) ;
 
 #endif
 
