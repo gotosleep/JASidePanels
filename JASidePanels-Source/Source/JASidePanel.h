363a5b510469c60ebeae349b8c75d11aa9581848
Brian McIntyre <brian@bmcintyre.com>
2013-05-11 19:26:44 -0400

    - Cleanup

--- JASidePanels-Source/Source/JASidePanel.h
+++ JASidePanels-Source/Source/JASidePanel.h
@@ -1,13 +1,12 @@
 //
 //  JASidePanel.h
-//  iBowen
 //
 //  Created by Brian McIntyre on 2013-05-11.
 //
 //
 
-#ifndef iBowen_JASidePanel_h
-#define iBowen_JASidePanel_h
+#ifndef _JASidePanel_h
+#define _JASidePanel_h
 
 #import "JASidePanelController.h"
 #import "UIViewController+JASidePanel.h"
