2d7323b89d95afd33aed33abc58b8395f856704e
Dave Anderson <david.anderson@robotsandpencils.com>
2013-07-29 13:59:05 -0600

    Add custom view for UIBarButtonItem modification

---
+++ JASidePanels/Source/JABarButtonView.h
@@ -0,0 +1,13 @@
+//
+//  JABarButtonView.h
+//  JASidePanels
+//
+//  Created by David Anderson on 2013-07-29.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JABarButtonView : UIView
+
+@end
---
+++ JASidePanels/Source/JABarButtonView.m
@@ -0,0 +1,31 @@
+//
+//  JABarButtonView.m
+//  JASidePanels
+//
+//  Created by David Anderson on 2013-07-29.
+//
+//
+
+#import "JABarButtonView.h"
+
+@implementation JABarButtonView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
