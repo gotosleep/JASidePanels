ffb69fc66c5bba586987d11751323f595850708a
Andrew Williams <awilliams@nextfaze.com>
2013-08-06 11:47:46 +0930

    added cocoa touch static library target

---
+++ JASidePanelsFramework/JASidePanelsFramework.h
@@ -0,0 +1,13 @@
+//
+//  JASidePanelsFramework.h
+//  JASidePanelsFramework
+//
+//  Created by Andrew Williams on 6/08/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JASidePanelsFramework : NSObject
+
+@end
---
+++ JASidePanelsFramework/JASidePanelsFramework.m
@@ -0,0 +1,13 @@
+//
+//  JASidePanelsFramework.m
+//  JASidePanelsFramework
+//
+//  Created by Andrew Williams on 6/08/13.
+//
+//
+
+#import "JASidePanelsFramework.h"
+
+@implementation JASidePanelsFramework
+
+@end
