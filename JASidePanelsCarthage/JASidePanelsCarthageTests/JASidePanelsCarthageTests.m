fb904d88e632fe4adab44ff990606df2fd311ab8
shuhei <kawashu0723@gmail.com>
2016-08-12 13:33:00 +0900

    carthageに対応

---
+++ JASidePanelsCarthage/JASidePanelsCarthageTests/JASidePanelsCarthageTests.m
@@ -0,0 +1,39 @@
+//
+//  JASidePanelsCarthageTests.m
+//  JASidePanelsCarthageTests
+//
+//  Created by Shuhei Kawaguchi on 2016/08/12.
+//  Copyright © 2016年 Shuhei Kawaguchi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JASidePanelsCarthageTests : XCTestCase
+
+@end
+
+@implementation JASidePanelsCarthageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
