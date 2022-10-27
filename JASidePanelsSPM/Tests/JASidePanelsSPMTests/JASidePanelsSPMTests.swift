eddc49b306d83c001e8bbfbca892a12c7aecc3a1
Keita Watanabe <watanabe.kit@gmail.com>
2022-10-27 13:17:40 +0900

    fix header

--- JASidePanelsSPM/Tests/JASidePanelsSPMTests/JASidePanelsSPMTests.swift
+++ JASidePanelsSPM/Tests/JASidePanelsSPMTests/JASidePanelsSPMTests.swift
@@ -6,6 +6,6 @@ final class JASidePanelsSPMTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(JASidePanelsSPM().text, "Hello, World!")
+        let vc = JASidePanelController()
     }
 }
